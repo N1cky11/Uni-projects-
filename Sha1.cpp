b2ec1295d2e6e5d83a9442aac7fbd80405261e74
N1cky11 <129217979+N1cky11@users.noreply.github.com>
2023-12-17 20:42:26 +0400

    Update Sha1.cpp

--- Sha1.cpp
+++ Sha1.cpp
@@ -1,30 +1,26 @@
 #include <iostream>
 #include <string>
-#include <bitset>
 #include <array>
 #include <vector>
 #include <algorithm>
-#include <iomanip>
+#include <sstream>
+#include <cstdint> 
+#include <iomanip> 
 
+uint32_t leftShift(const uint32_t& num, int shift) {
+    return (num << shift) | (num >> (32 - shift));
+}
 
-uint32_t leftShift(const uint32_t &num,int shift)
-  {
- return (num<<shift) | (num>>(32-shift) ) ;
-  }
-
-void convertStrToBinary(const std::string& str, std::vector<int>& converted)
-{
-    for(char c :str)
-    {
-        for(int i=7 ; i>=0 ; i--) converted.push_back( (c>>i)&1) ;
+void convertStrToBinary(const std::string& str, std::vector<int>& converted) {
+    for (char c : str) {
+        for (int i = 7; i >= 0; i--) converted.push_back((c >> i) & 1);
     }
 }
 
-void addBinaryStrSize(const uint32_t strSize, std::vector<int>& converted)
-{
+void addBinaryStrSize(const uint32_t strSize, std::vector<int>& converted) {
     std::array<int, 64> binary;
 
-    for (int i = 31; i >= 0; i--) {
+    for (int i = 63; i >= 0; i--) {
         binary[63 - i] = ((strSize >> i) & 1);
     }
 
@@ -47,42 +43,38 @@ inline uint32_t f3(uint32_t x, uint32_t y, uint32_t z) {
     return (x & y) ^ (x & z) ^ (y & z);
 }
 
-void wordsProcess(const std::vector<int>& converted, std::array<uint32_t, 80>& Words, int processed512)
-{
+void wordsProcess(const std::vector<int>& converted, std::array<uint32_t, 80>& Words, int processed512) {
     Words.fill(0);
 
-    for (int i = 0; i < 80; i++) {
+    for (int i = 0; i < 16; i++) {
+        for (int j = 0; j <= 31; j++) Words[i] |= converted[512 * processed512 + i * 32 + j] << (31 - j);
+    }
 
-        if (i < 16) {
-            for(int j=0 ; j<=31 ; j++) Words[i] |= converted[512 * processed512 + i*32+j]<<(31-j);
-        }
-        else {
-            Words[i] =leftShift( Words[i - 3] ^ Words[i - 8] ^ Words[i - 14] ^ Words[i - 16] , 1) ;
-        }
+    for (int i = 16; i < 80; i++) {
+        Words[i] = leftShift(Words[i - 3] ^ Words[i - 8] ^ Words[i - 14] ^ Words[i - 16], 1);
     }
 }
 
-std::string sha1(const std::string& str)
-{
+std::string sha1(const std::string& str) {
     std::vector<int> converted;
 
     convertStrToBinary(str, converted);
 
     converted.push_back(1);
 
-    while ( converted.size() % 512 != 448) {
+    while (converted.size() % 512 != 448) {
         converted.push_back(0);
     }
 
-    addBinaryStrSize(str.size(), converted);
+    uint32_t strSize = str.size() * 8;
+    addBinaryStrSize(strSize, converted);
 
     std::array<uint32_t, 80> Words;
-    int nBlocks512 = converted.size() / 512 , processed512 = 0;
+    int nBlocks512 = converted.size() / 512, processed512 = 0;
     uint32_t H0 = 0x67452301, H1 = 0xEFCDAB89, H2 = 0x98BADCFE, H3 = 0x10325476, H4 = 0xC3D2E1F0;
     uint32_t A, B, C, D, E;
 
-    while (processed512!=nBlocks512) {
-
+    while (processed512 != nBlocks512) {
         wordsProcess(converted, Words, processed512);
 
         A = H0;
@@ -95,42 +87,40 @@ std::string sha1(const std::string& str)
         uint32_t temp{};
 
         for (int i = 0; i < 80; i++) {
-
             if (i <= 19)
-                temp = leftShift(A,5) + f1(B, C, D) + E + Words[i] + K1;
+                temp = leftShift(A, 5) + f1(B, C, D) + E + Words[i] + K1;
             else if (i <= 39)
-                temp = leftShift(A,5) + f2(B, C, D) + E + Words[i] + K2;
+                temp = leftShift(A, 5) + f2(B, C, D) + E + Words[i] + K2;
             else if (i <= 59)
-                temp = leftShift(A,5) + f3(B, C, D) + E + Words[i] + K3;
+                temp = leftShift(A, 5) + f3(B, C, D) + E + Words[i] + K3;
             else if (i <= 79)
-                temp = leftShift(A,5) + f2(B, C, D) + E + Words[i] + K4;
+                temp = leftShift(A, 5) + f2(B, C, D) + E + Words[i] + K4;
 
             E = D;
             D = C;
-            C = leftShift(B,30);
+            C = leftShift(B, 30);
             B = A;
             A = temp;
-
-            H0 += A;
-            H1 += B;
-            H2 += C;
-            H3 += D;
-            H4 += E;
         }
+
+        H0 += A;
+        H1 += B;
+        H2 += C;
+        H3 += D;
+        H4 += E;
+
         processed512++;
     }
 
-   std::stringstream ss;
+    std::stringstream ss;
+    ss << std::hex << std::setw(8) << std::setfill('0') << H0 << std::setw(8) << H1 << std::setw(8) << H2
+       << std::setw(8) << H3 << std::setw(8) << H4;
 
-    ss << std::hex << H0 << H1 << H2 << H3 << H4;
-
-    return ss.str() ;
+    return ss.str();
 }
 
-int main()
-{
-    std::string str="abc" ;
-    std::cout<<sha1(str) ;
-
+int main() {
+    std::string str = "AbcD";
+    std::cout << sha1(str) << "\n";
     return 0;
 }
